f804459f696005b50832295816e42732ac398c0f
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-05 12:41:15 -0400

    test

--- setup.py
+++ setup.py
@@ -6,14 +6,18 @@ import sys
 
 
 class Tox(TestCommand):
+
     user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
     def initialize_options(self):
         TestCommand.initialize_options(self)
         self.tox_args = None
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import tox
@@ -45,7 +49,7 @@ setup(
     install_requires=[],
     tests_require=['tox', 'pytest', 'pytest-cov'],
     test_suite="test",
-    cmdclass = {'test': Tox},
+    cmdclass={'test': Tox},
     zip_safe=False,
     keywords='integer set span range intspan intrange intspanlist',
     classifiers=linelist("""
