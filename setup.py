90c6fb7cfb6b4136cb678388ac0043f9ff565aed
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-07 14:25:38 -0400

    1.3.9

--- setup.py
+++ setup.py
@@ -1,58 +1,34 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-import sys
+from codecs import open
 
 
-class Tox(TestCommand):
-
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
-
-
-def linelist(text):
+def lines(text):
     """
     Returns each non-blank line in text enclosed in a list.
+    See http://pypi.python.org/pypi/textdata for more sophisticated version.
     """
-    return [l.strip() for l in text.strip().splitlines() if l.split()]
+    return [l.strip() for l in text.strip().splitlines() if l.strip()]
 
 
 setup(
     name='intspan',
-    version='1.3.8',
+    version='1.3.9',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description="Sets of integers like 1,3-7,33",
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst', encoding='utf-8').read(),
     url='https://bitbucket.org/jeunice/intspan',
     license='Apache License 2.0',
     py_modules=['intspan'],
+    setup_requires=[],
     install_requires=[],
     tests_require=['tox', 'pytest', 'pytest-cov'],
     test_suite="test",
-    cmdclass={'test': Tox},
     zip_safe=False,
     keywords='integer set span range intspan intrange intspanlist',
-    classifiers=linelist("""
+    classifiers=lines("""
         Development Status :: 5 - Production/Stable
         Operating System :: OS Independent
         License :: OSI Approved :: Apache Software License
