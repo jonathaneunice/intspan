e57be6e0c90c7d3899dce6c5a36ecdb93f571378
Jonathan Eunice <jonathan.eunice@gmail.com>
2015-08-21 18:49:18 -0400

    1.4.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def lines(text):
 
 setup(
     name='intspan',
-    version='1.4.0',
+    version='1.4.1',
     author='Jonathan Eunice',
     author_email='jonathan.eunice@gmail.com',
     description="Sets of integers like 1,3-7,33",
